a71516ad6221d52d30fcedbd0b0d8c3708107958
Fhight <74805803+Fhight@users.noreply.github.com>
2023-08-31 22:52:58 -0600

    Tarea 6

---
+++ Tarea6/src/utils/AppUtils.js
@@ -0,0 +1,52 @@
+import { BONE, USER } from './ImageUtils';
+
+export const DATA = [
+  {
+    id: 1,
+    nombre: 'Pablo',
+    apellido: 'Juarez',
+    image: USER,
+  },
+  {
+    id: 2,
+    nombre: 'Ulises',
+    apellido: 'Rojas',
+    image: USER,
+  },
+  {
+    id: 3,
+    nombre: 'Hueso',
+    apellido: 'Reyes',
+    image: BONE,
+  },
+  {
+    id: 4,
+    nombre: 'Diego',
+    apellido: 'Ruiz',
+    image: USER,
+  },
+  {
+    id: 5,
+    nombre: 'Arturo',
+    apellido: 'Martinez',
+    image: USER,
+  },
+  {
+    id: 6,
+    nombre: 'Alan',
+    apellido: 'Garcia',
+    image: USER,
+  },
+  {
+    id: 7,
+    nombre: 'Angel',
+    apellido: 'Gallegos',
+    image: USER,
+  },
+  {
+    id: 8,
+    nombre: 'Pepe',
+    apellido: 'Velazquez',
+    image: USER,
+  },
+];
---
+++ Tarea6/src/utils/ImageUtils.js
@@ -0,0 +1,11 @@
+export const BACKGROUND_IMAGE = require('../../assets/background.png');
+
+export const USER = require('../../assets/user.png');
+
+export const BONE = require('../../assets/bone.png');
+
+export const EDIT_ICON = require('../../assets/icons/editIcon.png');
+
+export const VIEW_ICON = require('../../assets/icons/viewIcon.png');
+
+export const NO_VIEW_ICON = require('../../assets/icons/noViewIcon.png');
