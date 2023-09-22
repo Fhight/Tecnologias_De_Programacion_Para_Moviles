1de2cd891a33a43079ec4b4393516c868775eea2
Fhight <19121042@morelia.tecnm.mx>
2023-09-22 09:01:38 -0600

    Practica 4

--- App/App.js
+++ App/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import Main from './src/screens/Main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>    
       <StatusBar style="auto" />
+      <Main />
     </View>
   );
 }
@@ -14,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    
   },
 });
