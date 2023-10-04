cad3ccf84de8bd010f750a235c8996181f9a8bf3
Fhight <19121042@morelia.tecnm.mx>
2023-10-04 08:59:18 -0600

    Practica 9

--- App/App.js
+++ App/App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
+import Card from "./components/Card";
+import AppContextProvider from "./components/context/AppContext";
 
 export default function App() {
+  const person = {
+    id: 1,
+    name: "Pablo",
+    lastname: "Juarez",
+    age: 22,
+    location: "Morelia",
+    country: "Mexico",
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContextProvider>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Card person={person} />
+      </View>
+    </AppContextProvider>
   );
 }
 
@@ -16,5 +29,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: Constants.statusBarHeight,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
