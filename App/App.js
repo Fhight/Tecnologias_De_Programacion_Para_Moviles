270094707bf439aa532ee00bec580979395c4567
Fhight <19121042@morelia.tecnm.mx>
2023-10-06 08:58:15 -0600

    Practica10

--- App/App.js
+++ App/App.js
@@ -1,15 +1,49 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
+import "react-native-gesture-handler";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./src/screens/Home";
+import Login from "./src/screens/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthContextProvider from "./src/context/authContext";
+import Account from "./src/screens/Account";
+import { useAuthContext } from "./src/hooks/useAuthContext";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContextProvider>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Stack.Navigator initialRouteName="Login">
+            {/* {Layout()} */}
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Account" component={Account} />
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
+    </AuthContextProvider>
   );
 }
+
+const Layout = () => {
+  const { user } = useAuthContext();
+
+  if (!user) {
+    return <Stack.Screen name="Login" component={Login} />;
+  }
+
+  return (
+    <>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Account" component={Account} />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
