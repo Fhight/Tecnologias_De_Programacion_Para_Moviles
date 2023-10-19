b6163a61d4e044c7c75c035be442d33554a4f34f
Pablo Juarez <j.pablojuarez@live.com.mx>
2023-10-18 20:53:33 -0600

    Unidad3/expo

--- App/App.js
+++ App/App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Constants from "expo-constants";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import Camara from './src/components/Camara';
+import Home from './src/screens/Home';
+import { useState } from 'react';
+import VideoMaribanda from './src/components/VideoMaribanda';
 
 export default function App() {
+  const [camera, setCamera] = useState(false);
+  const [video, setVideo] = useState(false);
+
+  const handleCamera = () => {
+    setCamera((prev) => !prev);
+    setVideo(false);
+  };
+
+  const handleVideo = () => {
+    setVideo((prev) => !prev);
+    setCamera(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      {/* <Camara /> */}
+      <Home setCamera={handleCamera} setVideo={handleVideo} />
+      {camera && <Camara />}
+      {video && <VideoMaribanda />}
     </View>
   );
 }
@@ -14,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     paddingTop: Constants.statusBarHeight,
   },
 });
