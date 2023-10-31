b9f5a379ca44597e3e981cd1a0058794a4ce25f7
Pablo Juarez <j.pablojuarez@live.com.mx>
2023-10-30 22:33:19 -0600

    Tarea 2

--- App/App.js
+++ App/App.js
@@ -1,77 +1,110 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Constants from "expo-constants";
-import Button from "./src/components/Button";
-import { useReducer } from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import Button from './src/components/Button';
+import { useReducer, useState } from 'react';
 
 const CALCULATOR_TYPES = {
-  SELECT_NUMBER: "SELECT_NUMBER",
-  SELECT_OPERATOR: "SELECT_OPERATOR",
-  CALCULATE: "CALCULATE",
+  SELECT_NUMBER: 'SELECT_NUMBER',
+  SELECT_OPERATOR: 'SELECT_OPERATOR',
+  CALCULATE: 'CALCULATE',
+  CLEAN: 'CLEAN',
+  NEGATIVE: 'NEGATIVE',
 };
 
 const initialState = {
   displayNumber: 0,
-  operator: "",
+  operator: '',
   currentNumber: 0,
   previousNumber: 0,
+  operation: '',
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case CALCULATOR_TYPES.SELECT_NUMBER:
+      if (action.payload === '00') {
+        return {
+          ...state,
+          currentNumber: state.currentNumber * 100,
+          displayNumber: state.currentNumber * 100,
+        };
+      }
+      if (state.currentNumber !== 0) {
+        return {
+          ...state,
+          currentNumber: state.currentNumber + action.payload,
+          displayNumber: state.displayNumber + action.payload,
+        };
+      }
+      if (state.operator !== '' && state.operation === '') {
+        return {
+          ...state,
+          currentNumber: action.payload,
+          displayNumber: state.displayNumber + action.payload,
+        };
+      }
       return {
         ...state,
         currentNumber: action.payload,
-        displayNumber: state.currentNumber,
+        displayNumber: action.payload,
       };
     case CALCULATOR_TYPES.SELECT_OPERATOR:
       return {
-        ...state,
         operator: action.payload,
         previousNumber: state.currentNumber,
+        currentNumber: 0,
+        operation: state.displayNumber + action.payload,
+        displayNumber: 0,
       };
-
     case CALCULATOR_TYPES.CALCULATE:
       let result = 0;
       switch (state.operator) {
-        case "*":
+        case 'x':
           result = state.previousNumber * state.currentNumber;
-          return {
-            ...state,
-            displayNumber: result,
-            currentNumber: result,
-          };
-        case "-":
-          result = state.previousNumber - state.currentNumber;
-          return {
-            ...state,
-            displayNumber: result,
-            currentNumber: result,
-          };
-        case "+":
-          result = state.previousNumber + state.currentNumber;
-          return {
-            ...state,
-            displayNumber: result,
-            currentNumber: result,
-          };
-        case "/":
+          break;
+        case '-':
+          result =
+            parseInt(state.previousNumber) - parseInt(state.currentNumber);
+          break;
+        case '+':
+          result =
+            parseInt(state.previousNumber) + parseInt(state.currentNumber);
+          break;
+        case '/':
           result = state.previousNumber / state.currentNumber;
+          break;
+        case '%':
+          result = state.previousNumber % state.currentNumber;
+          break;
+        default:
           return {
             ...state,
-            displayNumber: result,
-            currentNumber: result,
+            operator: '',
           };
-
-        default:
           break;
       }
-    // return {
-    //   ...state,
-    //   operator: "",
-    //   displayNumber: state.previousNumber +
-    // }
+      return {
+        ...state,
+        displayNumber: result,
+        currentNumber: result,
+        operation: '',
+      };
+    case CALCULATOR_TYPES.CLEAN:
+      return {
+        displayNumber: 0,
+        operator: '',
+        currentNumber: 0,
+        previousNumber: 0,
+        operation: '',
+      };
+    case CALCULATOR_TYPES.NEGATIVE: {
+      return {
+        ...state,
+        currentNumber: state.currentNumber * -1,
+        displayNumber: state.currentNumber * -1,
+      };
+    }
     default:
       break;
   }
@@ -79,46 +112,69 @@ function reducer(state, action) {
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [smile, setSmile] = useState(false);
   const handleSelectNumber = (number) => {
-    dispatch({ type: "SELECT_NUMBER", payload: number });
+    dispatch({ type: 'SELECT_NUMBER', payload: number });
   };
 
   const handleSelectOperator = (operator) => {
-    dispatch({ type: "SELECT_OPERATOR", payload: operator });
+    dispatch({ type: 'SELECT_OPERATOR', payload: operator });
   };
 
   const handleCalculate = () => {
-    dispatch({ type: "CALCULATE" });
+    dispatch({ type: 'CALCULATE' });
   };
 
+  const handleClean = () => {
+    dispatch({ type: 'CLEAN' });
+  };
+
+  const handleNegativePositive = () => {
+    dispatch({ type: 'NEGATIVE' });
+  };
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>{state.operation}</Text>
+      <View
+        style={{ borderBottomWidth: 5, width: '70%', borderTopStartRadius: 20 }}
+      ></View>
       <Text style={styles.text}>{state.displayNumber}</Text>
+      <View style={{ borderBottomWidth: 5, width: '100%' }}></View>
       <View style={styles.row}>
-        <Button text={"9"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"8"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"7"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"+"} rol={"operator"} onPress={handleSelectOperator} />
+        <Button text={'C'} rol={'operator'} onPress={handleClean} />
+        <Button
+          text={'+/-'}
+          rol={'operator'}
+          onPress={handleNegativePositive}
+        />
+        <Button text={'%'} rol={'operator'} onPress={handleSelectOperator} />
+        <Button text={'/'} rol={'operator'} onPress={handleSelectOperator} />
       </View>
       <View style={styles.row}>
-        <Button text={"6"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"5"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"4"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"-"} rol={"operator"} onPress={handleSelectOperator} />
+        <Button text={'1'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'2'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'3'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'x'} rol={'operator'} onPress={handleSelectOperator} />
       </View>
       <View style={styles.row}>
-        <Button text={"3"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"2"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"1"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"*"} rol={"operator"} onPress={handleSelectOperator} />
+        <Button text={'4'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'5'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'6'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'-'} rol={'operator'} onPress={handleSelectOperator} />
       </View>
       <View style={styles.row}>
-        <Button text={"-"} rol={"operator"} onPress={handleSelectOperator} />
-        <Button text={"0"} rol={"number"} onPress={handleSelectNumber} />
-        <Button text={"/"} rol={"operator"} onPress={handleSelectOperator} />
-        <Button text={"="} rol={"operator"} onPress={handleCalculate} />
+        <Button text={'7'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'8'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'9'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'+'} rol={'operator'} onPress={handleSelectOperator} />
       </View>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <Button text={'0'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'00'} rol={'number'} onPress={handleSelectNumber} />
+        <Button text={'smile'} rol={'operator'} onPress={() => {}} />
+        <Button text={'='} rol={'operator'} onPress={handleCalculate} />
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -126,18 +182,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#f1f1ed',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    marginBottom: 2,
     paddingTop: Constants.statusBarHeight,
   },
   row: {
-    flexDirection: "row",
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
     gap: 10,
     marginTop: 10,
   },
   text: {
-    fontSize: 25,
-    fontWeight: "bold",
+    fontSize: 100,
+    fontWeight: 'bold',
   },
 });
--- App/src/components/Button.jsx
+++ App/src/components/Button.jsx
@@ -1,16 +1,22 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Button = ({ text, rol, onPress }) => {
   return (
-    <TouchableOpacity
-      style={[
-        styles.container,
-        rol === "number" ? styles.numberContainer : styles.operatorContainer,
-      ]}
-      onPress={() => onPress(text)}
-    >
-      <Text style={styles.text}>{text}</Text>
+    <TouchableOpacity style={styles.container} onPress={() => onPress(text)}>
+      <Text
+        style={[
+          styles.text,
+          rol === 'number' ? styles.numberContainer : styles.operatorContainer,
+        ]}
+      >
+        {text === 'smile' ? (
+          <FontAwesome5 name='smile-wink' size={24} color='green' />
+        ) : (
+          text
+        )}
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -19,21 +25,29 @@ export default Button;
 
 const styles = StyleSheet.create({
   container: {
-    width: 70,
-    height: 70,
-    backgroundColor: "yellow",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
+    width: 85,
+    height: 85,
+    backgroundColor: '#f1f1ed',
+    borderWidth: 3,
+    borderRadius: 50,
+    borderColor: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 5,
   },
   numberContainer: {
-    backgroundColor: "blue",
+    color: 'black',
   },
   operatorContainer: {
-    backgroundColor: "gray",
+    color: '#ed4545',
   },
   text: {
     fontSize: 25,
+    fontWeight: 'bold',
   },
 });
