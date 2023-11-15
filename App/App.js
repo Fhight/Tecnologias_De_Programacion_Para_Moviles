f7c8462d5da865b25a0a2ee18505359061dee654
Fhight <19121042@morelia.tecnm.mx>
2023-11-15 08:57:30 -0600

    Practica 5

--- App/App.js
+++ App/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Constants from "expo-constants";
+import { Provider, useSelector } from "react-redux";
+import { store } from "./src/store";
+import Counter from "./src/counter";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Counter />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
@@ -15,6 +20,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: Constants.statusBarHeight,
   },
 });
